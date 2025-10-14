37746e8bd51e3b26b89ecea203ea021fd74e3058
saiful871 <jannatultura892@gmail.com>
2025-10-14 19:27:07 +0600

    Update azan.js

--- SAIFUL/commands/azan.js
+++ SAIFUL/commands/azan.js
@@ -8,7 +8,7 @@ module.exports.config = {
   name: "azan",
   version: "3.5.0",
   hasPermssion: 0,
-  credits: "Akash + Saiful Edit",
+  credits: "Saiful",
   description: "প্রতিদিন আজান ও নামাজের সময় সব গ্রুপে অটো নোটিফিকেশন পাঠাবে",
   commandCategory: "Islamic",
   usages: "অটো রান",
